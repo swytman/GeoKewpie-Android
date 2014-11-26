b00bac9c19a0005c220b5d2a1bc92d3707972f87
Stepanov <stepanov.vm@gmail.com>
2014-11-26 20:24:43 +0300

    fix

--- db/migrate/20141126171402_add_place_to_all_g_ames.rb
+++ db/migrate/20141126171402_add_place_to_all_g_ames.rb
@@ -2,7 +2,7 @@ class AddPlaceToAllGAmes < ActiveRecord::Migration
   def change
     ids = [4, 3, 2]
     Champ.where(id: ids).each do |champ|
-      champ.games.where.update_all(place: 'ФОК "Радуга"')
+      champ.games.update_all(place: 'ФОК "Радуга"')
     end
     ids = [1, 6, 5]
     Champ.where(id: ids).each do |champ|
